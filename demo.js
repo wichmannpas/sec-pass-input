b60a34d62c186264ad76e16e8aafde6f8b4025c5
Pascal Wichmann <wichmann@informatik.uni-hamburg.de>
2021-06-23 09:02:48 +0200

    Complete password input implementation

--- demo.js
+++ demo.js
@@ -1,6 +1,24 @@
 (function () {
+  function byteArrayToString (array) {
+    let result = ''
+    for (let i = 0; i < array.byteLength; i++) {
+      result += String.fromCharCode(array[i])
+    }
+    return result
+  }
+
   const demoInput = document.getElementById('demo-input')
   const demo = window.createSecurePasswordInput(demoInput)
 
-  console.log(demo)
+  const demoDisplay = document.getElementById('secure-password-display')
+  demoInput.oninput = passwordData => {
+    demoDisplay.innerText = byteArrayToString(passwordData.value)
+  }
+
+  const nativeInput = document.getElementById('native-input')
+  const nativeDisplay = document.getElementById('native-password-display')
+  nativeInput.onchange = event => {
+    nativeDisplay.innerText = event.target.value
+  }
+  nativeInput.oninput = nativeInput.onchange
 })();
--- secure-password-input.js
+++ secure-password-input.js
@@ -1,7 +1,11 @@
+// TODO: allow to provide a max length
+// TODO: allow to disable dot display
+
 (function () {
   const CONTROL_KEYS = [
     'Backspace', 'Delete',
-    'ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft',
+    'ArrowLeft', 'ArrowRight',
+    'Home', 'End',
   ]
   const ALLOWED_KEYS = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -15,13 +19,9 @@
     'ä', 'Ä', 'ö', 'Ö', 'ü', 'Ü', 'ß',
     // TODO: more special characters
 
-    '*', '/', '\\', '(', ')', '[', ']',
+    ' ', '*', '/', '\\', '(', ')', '[', ']', '#', '@', '!', '.', ',', ';', ':', "'", '"', '%', '$', '~', '+', '-',
   ]
   const HANDLED_KEYS = CONTROL_KEYS + ALLOWED_KEYS
-
-  // TODO: allow to provide a max length
-  // TODO: change handler/onchange callback
-  // TODO: allow to disable dot display
 
   function overwriteArray (array) {
     for (let i = 0; i < array.byteLength; i++) {
@@ -32,7 +32,6 @@
   window.createSecurePasswordInput = (input) => {
     const passwordData = {
       value: new Uint8Array(0),
-      input
     }
     let cursorPosition = -1
 
@@ -57,35 +56,45 @@
 
     updateDotDisplay()
 
-
     // listen to keys
     input.addEventListener('keydown', event => {
       if (HANDLED_KEYS.indexOf(event.key) === -1) {
         return
       }
-      if (event.ctrlKey || event.altKey) {
+      // ignore ctrl and alt keys, except if it is ctrl-arrow or ctrl-backspace
+      if (
+        (event.ctrlKey && !(event.key === 'ArrowLeft' || event.key === 'ArrowRight'
+          || event.key === 'Backspace' || event.key === 'Delete'))
+        || event.altKey) {
         return
       }
       event.preventDefault()
 
       if (ALLOWED_KEYS.indexOf(event.key) >= 0) {
-        // TODO: cursor support – insert the character somewhere else!
-
         // update value
         const newValue = new Uint8Array(passwordData.value.byteLength + 1)
-        newValue.set(passwordData.value)
-        newValue[newValue.length - 1] = event.key.charCodeAt(0)
+        const beforeCursor = new Uint8Array(passwordData.value.buffer.slice(0, cursorPosition + 1))
+        const afterCursor = new Uint8Array(passwordData.value.buffer.slice(cursorPosition + 1, passwordData.value.byteLength))
+        newValue.set(beforeCursor)
+        newValue.set(afterCursor, cursorPosition + 2)
+        newValue[cursorPosition + 1] = event.key.charCodeAt(0)
 
-        // overwrite old value with zeros
+        // overwrite old and temporary value with zeros
         overwriteArray(passwordData.value)
+        overwriteArray(beforeCursor)
+        overwriteArray(afterCursor)
 
         passwordData.value = newValue
         cursorPosition++
+      } else if (event.key === 'Home' || (event.ctrlKey && event.key === 'ArrowLeft')) {
+        cursorPosition = -1
       } else if (event.key === 'ArrowLeft') {
         if (cursorPosition < 0) {
           return
         }
         cursorPosition--
+      } else if (event.key === 'End' || (event.ctrlKey && event.key === 'ArrowRight')) {
+        cursorPosition = passwordData.value.byteLength - 1
       } else if (event.key === 'ArrowRight') {
         if (cursorPosition >= passwordData.value.byteLength - 1) {
           return
@@ -96,12 +105,73 @@
           return
         }
 
-        cursorPosition--
+        // update value
+        let newLength = passwordData.value.byteLength - 1
+        if (event.ctrlKey) {
+          newLength = passwordData.value.byteLength - cursorPosition - 1
+        }
+        const newValue = new Uint8Array(newLength)
+        const beforeCursor = new Uint8Array(passwordData.value.buffer.slice(0, cursorPosition))
+        const afterCursor = new Uint8Array(passwordData.value.buffer.slice(cursorPosition + 1, passwordData.value.byteLength))
+        if (event.ctrlKey) {
+          newValue.set(afterCursor)
+        } else {
+          newValue.set(beforeCursor)
+          newValue.set(afterCursor, cursorPosition)
+        }
 
-        // TODO!
+        // overwrite old and temporary value with zeros
+        overwriteArray(passwordData.value)
+        overwriteArray(beforeCursor)
+        overwriteArray(afterCursor)
+
+        passwordData.value = newValue
+        if (event.ctrlKey) {
+          cursorPosition = -1
+        } else {
+          cursorPosition--
+        }
+      } else if (event.key === 'Delete') {
+        if (cursorPosition >= passwordData.value.byteLength - 1) {
+          return
+        }
+
+        // update value
+        let newLength = passwordData.value.byteLength - 1
+        if (event.ctrlKey) {
+          newLength = cursorPosition + 1
+        }
+        const newValue = new Uint8Array(newLength)
+        const beforeCursor = new Uint8Array(passwordData.value.buffer.slice(0, cursorPosition + 1))
+        const afterCursor = new Uint8Array(passwordData.value.buffer.slice(cursorPosition + 2, passwordData.value.byteLength))
+        if (event.ctrlKey) {
+          newValue.set(beforeCursor)
+        } else {
+          if (cursorPosition >= 0) {
+            newValue.set(beforeCursor)
+            newValue.set(afterCursor, cursorPosition + 1)
+          } else {
+            newValue.set(afterCursor)
+          }
+        }
+
+        // overwrite old and temporary value with zeros
+        overwriteArray(passwordData.value)
+        overwriteArray(beforeCursor)
+        overwriteArray(afterCursor)
+
+        passwordData.value = newValue
       } else {
-        // TODO
-        console.log(event)
+        console.error('Unhandled event/key:')
+        console.error(event)
+        return
+      }
+
+      if (typeof input.oninput === 'function') {
+        input.oninput(passwordData)
+      }
+      if (typeof input.onchange === 'function') {
+        input.onchange(passwordData)
       }
 
       updateDotDisplay()
